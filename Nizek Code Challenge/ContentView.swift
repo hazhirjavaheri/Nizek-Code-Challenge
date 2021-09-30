d9be939ea4a01d122002ef1b502ba13e16e09b67
hazhirjavaheri <89680494+hazhirjavaheri@users.noreply.github.com>
2021-09-30 18:47:21 +0330

    Initial Commit

---
+++ Nizek Code Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nizek Code Challenge
+//
+//  Created by Hajir Javaheri on 7/8/1400 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nizek Code Challenge/NizekCodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nizek_Code_ChallengeApp.swift
+//  Nizek Code Challenge
+//
+//  Created by Hajir Javaheri on 7/8/1400 AP.
+//
+
+import SwiftUI
+
+@main
+struct NizekCodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
